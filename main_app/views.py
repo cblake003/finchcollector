8caaa2d488997c25e70e3b72d025df995bcd0755
cblake003 <cameronburtner@gmail.com>
2023-09-11 16:24:51 -0700

    finished part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+finches = [
+  {'name': 'goldfinch', 'breed': 'european', 'description': 'fat little brown one', 'age': 3},
+  {'name': 'finch', 'breed': 'american', 'description': 'fat little yellow one', 'age': 2},
+]
+
+def home(request):
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {
+        'finches': finches
+    })
