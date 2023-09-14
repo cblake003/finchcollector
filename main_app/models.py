4956a948bc9611ac945366fa1cbc0929de3f35cb
cblake003 <cameronburtner@gmail.com>
2023-09-14 13:14:57 -0700

    added feeding to details

--- main_app/models.py
+++ main_app/models.py
@@ -1,8 +1,33 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
 
 class Finch(models.Model):
     name = models.CharField(max_length=100)
     area = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
+
+    def __str__(self):
+        return f'{self.name} ({self.id})'
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField('feeding date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
